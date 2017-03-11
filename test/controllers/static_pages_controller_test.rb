63d1bb364ddc4201a10a635d3e4db1c432d6d86e
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-11 14:22:04 +0100

    change root route to home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   setup do
     @title_long = "| Ruby on Rails Tutorial Sample App"
   end
