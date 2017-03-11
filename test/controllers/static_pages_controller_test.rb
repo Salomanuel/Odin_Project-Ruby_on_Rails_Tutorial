fd4bc209f21f2c3df542e81946e55cb0d63dd80d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-11 14:05:10 +0100

    add titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @title_long = "| Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home #{@title_long}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help #{@title_long}"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
+    assert_select "title", "About #{@title_long}"
   end
+
 end
