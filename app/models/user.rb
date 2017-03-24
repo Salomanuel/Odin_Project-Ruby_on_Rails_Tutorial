033c14a98f0143079e0d5e5d90c6725b43484a4b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-24 23:34:16 +0100

    add validation for password presence and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
 	before_save { self.email = email.downcase }
 	VALID_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :name , presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 },
-										format: { with: VALID_email_regex},
-										uniqueness: { case_sensitive: false }
+	validates :name , 		presence: true, 
+												length: { maximum: 50 }
+	
+	validates :email, 		presence: true, 
+												length: { maximum: 255 },
+												format: { with: VALID_email_regex},
+												uniqueness: { case_sensitive: false }
+	
+	validates :password, 	presence: true, 
+												length: { minimum: 6 }
 	has_secure_password
 end
