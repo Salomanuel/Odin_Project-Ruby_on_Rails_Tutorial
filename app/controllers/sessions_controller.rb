7baa330a0889e3ae91e207bf6d07b8d6de571cc9
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-20 17:42:17 +0200

    building the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user and user.authenticate(params[:session][:password])
+  		# log in and redirect
+  	else
+  		# error
+  		flash[:danger] = "Invalid email/password"
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  end
 end
