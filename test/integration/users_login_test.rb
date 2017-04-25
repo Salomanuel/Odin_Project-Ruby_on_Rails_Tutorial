b7b76cb396cde1a3c3b99c455bd970690b543797
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-25 22:23:24 +0200

    comment the test and fix the flash with flash.now

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	test "login with invalid information" do
+		# visit the login path
+		get login_path
+		# verify that the new sessions form renders properly
+		assert_template 'sessions/new'	
+		# post to the sessions path with an invalid params hash
+		post login_path, params: { session: { email:"", password:"" } }
+		# verify that the new session form gets re-rendered
+		assert_template 'sessions/new'
+		# and that a flash message appears
+		assert_not flash.empty?
+		# visit another page (home)
+		get root_path
+		# verify that the flash is not present
+		assert flash.empty?
+	end
 end
