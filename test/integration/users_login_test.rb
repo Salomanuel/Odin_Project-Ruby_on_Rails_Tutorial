6caa8132713569e75b3341f56e737f01d43eac47
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-26 12:34:06 +0200

    add fixture for test user and add test for valid login information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:michael)
+	end
+
 	test "login with invalid information" do
 		# visit the login path
 		get login_path
@@ -16,5 +20,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		get root_path
 		# verify that the flash is not present
 		assert flash.empty?
+	end
+
+	test "login with valid information" do
+		get login_path
+		post login_path, params: { session: { email: 		@user.email,
+																					password: 'password' } }
+		# gets redirected to the user page
+		assert_redirected_to @user
+		# actually visit the target page
+		follow_redirect!
+		assert_template 'users/show'
+		# there are ZERO login paths on the page
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", logout_path
+		assert_select "a[href=?]", user_path(@user)
 	end
 end
