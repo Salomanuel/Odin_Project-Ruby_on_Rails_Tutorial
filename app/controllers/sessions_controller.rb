e5092dbb382a2b4a1f567b694148d58556335353
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-28 13:49:19 +0200

    begin the create action in the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user and user.authenticate(params[:session][:password])
+  		# log user in
+  	else
+  		# create and error message
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  end
 end
