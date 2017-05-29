5d5ba2aa8083c1d10e5aedc995029d6f8fdfe3ce
Salomanuel <salomanue.aux+code@gmail.com>
2017-05-29 12:25:27 +0200

    implement the dynamic titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
+  	# @base_title = 
+		@title       = "Home"
   end
 
   def help
+  	@title = "Help"
   end
 
   def about
+  	@title = "About"
+  end
+
+  def base_title
+  	@base_title = "| Ruby on Rails Tutorial Sample App"
   end
 end
