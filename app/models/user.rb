226647f6945051f63e9b51ade24cad68fcceb6c4
Salomanuel <salomanue.aux+code@gmail.com>
2017-05-31 12:09:51 +0200

    in User model, format better validations and add uniqueness, add also a test for it in models/user_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\+\.a-z_-]+@[a-z]+\.[a-z.]+\z/i
-	validates(:name,  presence: true, length: { maximum: 50 } )
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+	validates(:name,  presence: true, 
+										length: { maximum: 50 } )
+  validates :email, presence: true, 
+  									length: { maximum: 255 }, 
+  									format: { with: VALID_EMAIL_REGEX },
+  									uniqueness: { case_sensitive: false }
 end
