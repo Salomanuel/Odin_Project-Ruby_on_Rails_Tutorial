cfb86b061366a13ecd8e3ee04f85a5c38a8c5894
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-05 12:28:30 +0200

    in the User controller, add strong parameters with #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user]) # not final
+  	@user = User.new(user_params)
   	if @user.save
   		render html: "user saved"
   	else
@@ -18,5 +18,11 @@ class UsersController < ApplicationController
   	end
   end
 
-  
+  private
+  	def user_params
+  		params.require(:user).permit(	:name, 
+  																	:email, 
+  																	:password,
+  																	:password_confirmation)
+  	end
 end
