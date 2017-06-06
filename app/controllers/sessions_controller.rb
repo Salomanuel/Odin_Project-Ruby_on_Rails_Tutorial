c06620f1e5134dc242029c7e79545a792141d150
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-06 12:48:46 +0200

    in the sessions_controller#create build the login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user and user.authenticate(params[:session][:password])
+  		# log in and redirect to user show
+  		flash[:success] = "logged in"
+  		render 'new'
+  		# render html: "login"
+  	else
+  		flash[:danger]  = "Invalid email/password combination"
+  		# error message
+			render 'new'
+			# render html: "error"
+		end
+  end
+
+  def destroy
+  end
 end
