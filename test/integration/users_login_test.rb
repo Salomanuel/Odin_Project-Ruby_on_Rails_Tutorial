a6565dde3def0b1cbc3207732c3d945a924858fa
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-08 15:21:29 +0200

    in users_login_test add a test for the layout changes after logging in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:michael) # from fixtures
+	end
+
 	test "flash message should disappear" do
 		get login_path
 		assert_template 'sessions/new'
@@ -10,4 +14,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		get root_path
 		assert flash.empty?
 	end	
+
+	test "layout changes with loggin" do
+		get login_path
+		assert_template 'sessions/new'
+		assert_select "a[href=?]", login_path
+		post login_path, params: { session: {email: @user.email, password: "password" } }
+		assert_redirected_to @user
+		follow_redirect! 
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", logout_path
+		assert_select "a[href=?]", user_path(@user)
+	end
 end
