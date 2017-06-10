68d96af9c3d41e63ee14ed979eb05bca74afe35b
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-10 21:18:47 +0200

    to integration/users_login_test add login with and without remembering

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,6 +35,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", login_path
 		assert_select "a[href=?]", logout_path, 			count: 0
 		assert_select "a[href=?]", user_path(@user), 	count: 0
+	end
 
+	test "login with remembering" do
+		log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+	end
+
+	test "login without remembering" do
+		# log in to set the cookie
+		log_in_as(@user, remember_me: '1')
+		# log in again and verify that the cookie is delete
+		log_in_as(@user, remember_me: '0')
+		assert_empty cookies['remember_token']
 	end
 end
