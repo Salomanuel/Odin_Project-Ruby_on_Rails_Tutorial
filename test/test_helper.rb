0bd0607cce337fda73441558494e989690c3a25f
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-10 21:18:07 +0200

    in test_helper add not just one BUT TWO #log_in_as

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,28 @@ class ActiveSupport::TestCase
   def is_logged_in?
   	!session[:user_id].nil?
   end
+
+  # logs in as a particular user
+  def log_in_as(user)
+  	session[:user_id] = user.id
+  end
 end
+
+class ActionDispatch::IntegrationTest
+=begin 
+	Because it’s located inside the 
+	ActionDispatch::IntegrationTest class, 
+	this is the version of log_in_as that will 
+	be called inside integration tests.
+=end
+
+	# logs in as a particular user
+	def log_in_as(user, password: 'password', remember_me: '1')
+		post login_path, params: 
+		{ session: { 	email:  		 user.email,
+									password: 	 password,
+									remember_me: remember_me } }
+	end
+end
+
+
