3b84307dee153f72853346c9243f1a3f93f73ebe
Salomanuel <salomanuel.aux+code@gmail.com>
2017-06-13 16:53:40 +0200

    in users_edit_test add an 'unsuccessful edit' test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,16 +5,31 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		@user = users(:michael)
 	end
 
+	test "unsuccessful edit" do
+		log_in_as(@user)
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		name  = ""
+		email = ""
+		patch user_path(@user), params:
+			{user: { name:  name,
+							 email: email}}
+		assert_template 'users/edit'
+		assert_select "div.alert", "The form contains 3 errors."
+	  assert_not_equal name,  @user.name
+	  assert_not_equal email, @user.email
+
+	end
+
 	test "successful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		name  = "Foo Bar"
 		email = "foo@bar.baz"
 		patch user_path(@user), params: 
 		{ user: { name: 	name, 
-							email: 	email,
-							password: "foobar",
-							password_confirmation: "foobar" }}
+							email: 	email }}
 		assert_not flash.empty?
 		assert_redirected_to @user
 		@user.reload
