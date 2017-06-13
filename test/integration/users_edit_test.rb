ad203958df11df202c79f1825715f08bbd9ed83a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-06-13 17:11:58 +0200

    in users_edit_test add tests to catch the restrictions while not logged in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -12,8 +12,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		name  = ""
 		email = ""
 		patch user_path(@user), params:
-			{user: { name:  name,
-							 email: email}}
+			{user: { name:  name, email: email}}
 		assert_template 'users/edit'
 		assert_select "div.alert", "The form contains 3 errors."
 	  assert_not_equal name,  @user.name
@@ -28,12 +27,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		name  = "Foo Bar"
 		email = "foo@bar.baz"
 		patch user_path(@user), params: 
-		{ user: { name: 	name, 
-							email: 	email }}
+		{ user: { name: 	name, email: 	email }}
 		assert_not flash.empty?
 		assert_redirected_to @user
 		@user.reload
 		assert_equal name, 	@user.name
 		assert_equal email, @user.email
+	end
+
+	test "should redirect edit when not logged in" do
+		get edit_user_path(@user)
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
+
+	test "should redirect update when not logged in" do
+		patch user_path(@user), params: 
+		{user: { name: @user.name, email: @user.email } }
+		assert_not flash.empty?
+		assert_redirected_to login_url
 	end
 end
