b667f89d0f6479515b0d90b6e5e7a17f664af892
Salomanuel <salomanuel.aux+code@gmail.com>
2017-06-13 15:38:51 +0200

    in the User model, skip validations for password if it's empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
 
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\d\+\.a-z_-]+@[a-z]+\.[a-z.]+\z/i
-	validates(:name,  		presence: true, 
-												length: { maximum: 50 } )
-  validates :email, 		presence: true, 
-  											length: { maximum: 255 }, 
-  											format: { with: VALID_EMAIL_REGEX },
+	validates(:name,  		presence: 	true, 
+												length: 		{ maximum: 50 } )
+  validates :email, 		presence: 	true, 
+  											length: 		{ maximum: 255 }, 
+  											format: 		{ with: VALID_EMAIL_REGEX },
   											uniqueness: { case_sensitive: false }
-	validates	:password, 	presence: true,
-												length: { minimum: 6 }
-
+	validates	:password, 	presence: 	true,
+												length: 		{ minimum: 6 },
+												allow_nil: 	true
 	has_secure_password
 
 	# returns the hash digest of the given string
