a419d492c710312591cac38ceb5aef91847ce2ac
Salomanuel <salomanuel.aux+code@gmail.com>
2017-06-14 11:16:47 +0200

    in the Sessions controller change the call to #redirect_to(@user) to #redirect_back_or(@user) instead, that's the new method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 			log_in @user 	   # methods in sessions_helper
 			params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
   		flash[:success] = "logged in"
-			redirect_to @user	
+			redirect_back_or(@user)	# (@user is used as the default argument of #redirect_back_or if there is no another path)
   	else
   		flash.now[:danger]  = "Invalid email/password combination"
 			render 'new'
