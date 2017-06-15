bb916cebbbb02014044b58d30d3c54594ec90106
Salomanuel <salomanuel.aux+code@gmail.com>
2017-06-15 16:28:55 +0200

    in users_controller_test, add a test to check if everyone can be an admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,4 +15,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test "should not allow the admin attribute to be edited via THE WEB" do
+  	log_in_as(@other_user)
+  	assert_not @other_user.admin?
+  	patch user_path(@other_user), params: {
+  			user: { password:  							"password",
+  							password_confirmation:  "password",
+  							admin: 									true } }
+  	assert_not @other_user.reload.admin?
+  end
 end
