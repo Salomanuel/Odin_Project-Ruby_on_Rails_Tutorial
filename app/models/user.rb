3940ec0d560cdd299f31ec496c151aadfa324543
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-01 17:44:46 +0200

    add #create_activation_digest and :activation_token accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-
+	attr_accessor :remember_token, :activation_token
 	before_save { self.email = email.downcase }
+	before_create :create_activation_digest
+	
 	VALID_EMAIL_REGEX = /\A[\d\+\.a-z_-]+@[a-z]+\.[a-z.]+\z/i
 	validates(:name,  		presence: 	true, 
 												length: 		{ maximum: 50 } )
@@ -40,4 +41,10 @@ class User < ApplicationRecord
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	private
+		def create_activation_digest
+			self.activation_token  = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
