57875f8fec804e917f3e16b08d964cd060b1e1a1
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-04 12:26:38 +0200

    change the behaviour upon log in
    now the user has to be activated before being able to log in, and if it's not a flash message tells the user to activate the damn account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,18 @@ class SessionsController < ApplicationController
     # user could be a @class variable for the sake of testing the way listing 9.27 does
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user and user.authenticate(params[:session][:password])
-			log_in user 	   # methods in sessions_helper
-			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		flash[:success] = "logged in"
-			redirect_back_or(user)	# (@user is used as the default argument of #redirect_back_or if there is no other path)
+      if user.activated?
+  			log_in user 	   # method in sessions_helper
+  			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    		flash[:success] = "logged in" # not sure if this can remain here
+  			redirect_back_or(user)	# (@user is used as the default argument of #redirect_back_or if there is no other path)
       # #redirect_back_or comes from sessions_helper
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
   	else
   		flash.now[:danger]  = "Invalid email/password combination"
 			render 'new'
