a7c1baa1fa3da8aae7671f6553b97da37ac57508
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-05 11:58:21 +0200

    create #create_reset_digest and #send_password_reset_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save 	:downcase_email
 	before_create :create_activation_digest
 
@@ -44,6 +44,8 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, nil)
 	end
 
+# ACCOUNT ACTIVATION
+
 	# activates an account
 	def activate
 		update_columns(activated: true, activated_at: Time.zone.now)
@@ -58,6 +60,19 @@ class User < ApplicationRecord
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
 		# the call for self used to be to @user
+	end
+
+# PASSWORD RESET
+	# sets the password reset attributes
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# sends password reset email
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
 	end
 
 	private
