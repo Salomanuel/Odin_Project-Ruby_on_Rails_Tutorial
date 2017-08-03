2c04ebd38716821649ef36af8e7d2e925332e8d7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-03 17:43:31 +0200

    implement Micropost order (descending) and the dependency on their User

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -25,4 +25,16 @@ class MicropostTest < ActiveSupport::TestCase
 		@micropost.content = "a" * 141
 		assert_not @micropost.valid?
 	end
+
+	test "order should be most recent first" do
+		assert_equal microposts(:most_recent), Micropost.first
+	end
+
+	test "associated microposts should be destroyed" do
+		@user.save
+		@user.microposts.create!(content: "Lorem ipsum")
+		assert_difference 'Micropost.count', -1 do
+			@user.destroy
+		end
+	end
 end
