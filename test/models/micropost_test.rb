e5605e1ae6490432687343473722d08441b5a01d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-04 12:25:14 +0200

    add fixture's Microposts User

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -33,7 +33,7 @@ class MicropostTest < ActiveSupport::TestCase
 	test "associated microposts should be destroyed" do
 		@user.save
 		@user.microposts.create!(content: "Lorem ipsum")
-		assert_difference 'Micropost.count', -1 do
+		assert_difference 'Micropost.count', -35 do
 			@user.destroy
 		end
 	end
